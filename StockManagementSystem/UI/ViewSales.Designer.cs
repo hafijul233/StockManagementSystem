b44057efff744d7d7d2eec8ba9b778d6e1ac34c1
Hafijul Islam <hafijul233@outlook.com>
2018-08-09 05:43:09 +0600

    Alll Completed

--- StockManagementSystem/UI/ViewSales.Designer.cs
+++ StockManagementSystem/UI/ViewSales.Designer.cs
@@ -29,20 +29,147 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewSales));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.StartDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EndDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.SalesListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SearchButton);
+            this.groupBox1.Controls.Add(this.SalesListView);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.EndDatePicker);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.StartDatePicker);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1104, 682);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "View Sales Between two Dates";
+            // 
+            // StartDatePicker
+            // 
+            this.StartDatePicker.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartDatePicker.CustomFormat = "yyyy-MM-dd";
+            this.StartDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.StartDatePicker.Location = new System.Drawing.Point(444, 45);
+            this.StartDatePicker.Name = "StartDatePicker";
+            this.StartDatePicker.Size = new System.Drawing.Size(290, 30);
+            this.StartDatePicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Start Date From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Start Date From:";
+            // 
+            // EndDatePicker
+            // 
+            this.EndDatePicker.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EndDatePicker.CustomFormat = "yyyy-MM-dd";
+            this.EndDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.EndDatePicker.Location = new System.Drawing.Point(444, 107);
+            this.EndDatePicker.Name = "EndDatePicker";
+            this.EndDatePicker.Size = new System.Drawing.Size(290, 30);
+            this.EndDatePicker.TabIndex = 2;
+            // 
+            // SalesListView
+            // 
+            this.SalesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader4});
+            this.SalesListView.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalesListView.FullRowSelect = true;
+            this.SalesListView.GridLines = true;
+            this.SalesListView.Location = new System.Drawing.Point(23, 229);
+            this.SalesListView.Margin = new System.Windows.Forms.Padding(20);
+            this.SalesListView.Name = "SalesListView";
+            this.SalesListView.Size = new System.Drawing.Size(1058, 430);
+            this.SalesListView.TabIndex = 66;
+            this.SalesListView.UseCompatibleStateImageBehavior = false;
+            this.SalesListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "SI";
+            this.columnHeader1.Width = 110;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Item";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 340;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Quantity";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 295;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(570, 161);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(164, 57);
+            this.SearchButton.TabIndex = 67;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
             // 
             // ViewSales
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1104, 682);
+            this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1126, 738);
             this.Name = "ViewSales";
-            this.Text = "ViewSales";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "View Sales Between two Dates";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker EndDatePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker StartDatePicker;
+        private System.Windows.Forms.ListView SalesListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button SearchButton;
     }
 }
--- StockManagementSystem/UI/ViewSales.cs
+++ StockManagementSystem/UI/ViewSales.cs
@@ -2,19 +2,78 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StockManagementSystem.DAL;
+using StockManagementSystem.Models;
 
 namespace StockManagementSystem.UI
 {
     public partial class ViewSales : Form
     {
+        SqlConnection connect = new SqlConnection(DBConnection.connection());
+
+        int si = 1;
+
         public ViewSales()
         {
             InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string startDate = StartDatePicker.Text;
+            string endDate = EndDatePicker.Text;
+
+            List<StockReport> ItemSalesList = new List<StockReport>();
+
+            SalesListView.Clear();
+            ItemSalesList.Clear();
+
+            string query = @"SELECT ItemName, StockOutQuantity FROM StockReport WHERE SalesFlag = 1";
+
+            SqlCommand command = new SqlCommand(query, connect);
+
+            if (connect.State == ConnectionState.Open)
+                connect.Close();
+            else
+                connect.Open();
+
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            SalesListView.Columns.Add("SI");
+            SalesListView.Columns.Add("Item Name");
+            SalesListView.Columns.Add("Quantity");
+
+
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    string[] arr = new string[3];
+                    ListViewItem itm;
+
+                    arr[0] = si.ToString();
+                    arr[1] = dataReader["ItemName"].ToString();
+                    arr[2] = dataReader["StockOutQuantity"].ToString();
+               
+                    itm = new ListViewItem(arr);
+                    SalesListView.Items.Add(itm);
+
+                    si++;
+                }
+            }
+            else
+            {
+                
+            }
+
+            connect.Close();
+            
         }
     }
 }
