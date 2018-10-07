3b0577a2a6a91fa2fead3285985670adb7d690d0
Hafijul Islam <hafijul233@gmail.com>
2018-10-08 00:55:21 +0600

    DbContest Added

--- DatabaseContext/StockDatabase.cs
+++ DatabaseContext/StockDatabase.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DatabaseContext
 {
-    public class StockDatabase
+    public class StockDatabase : DbContext
     {
+        
     }
 }
