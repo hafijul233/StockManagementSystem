b249e4bc4860e2912b2ad0485064d6925fcc1212
Hafijul Islam <hafijul233@gmail.com>
2018-10-27 20:35:59 +0600

    Graphics Update

--- StockManagementSystem/Program.cs
+++ StockManagementSystem/Program.cs
@@ -15,9 +15,15 @@ namespace StockManagementSystem
         [STAThread]
         static void Main()
         {
+        	if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginUi());
         }
+        
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
